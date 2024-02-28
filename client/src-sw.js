24dcbe1d8e1a1f4ad81fdc768426738001f46047
Bdoherty1 <bdoherty988@gmail.com>
2024-02-27 20:24:56 -0600

    add a click event handler on the  element and for appinstalled

--- client/src-sw.js
+++ client/src-sw.js
@@ -24,7 +24,20 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
 // TODO: Implement asset caching
+registerRoute(
+  ({ request }) => request.mode === 'navigate', pageCache);
+new CacheFirst({
+  cacheName: '',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxEntries: 50,
+      maxAgeSeconds: 7 * 24 * 60 * 60, //7days
+    })
+  ]
+})
+
 registerRoute();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,28 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
-
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    event.preventDefault();
+    // Stash the event so it can be triggered later
+    deferredPrompt = event;
+    // Show the install button
+    butInstall.style.display = 'block';
+  });
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  butInstall.style.display = 'none';
+  deferredPrompt.prompt();
 
+  const choiceResult = await deferredPrompt.userChoice;
+  if (choiceResult.outcome === 'accepted') {
+    console.log('User accepted the install prompt')
+  } else{
+    console.log('User dismissed the install prompt')
+  }
+  deferredPrompt = null;
+});
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('App installed successfully');
+});
